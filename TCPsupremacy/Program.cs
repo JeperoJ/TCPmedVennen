26c26880d319b57d62f03a4edce10062fc31db19
JeperoJ <55749013+JeperoJ@users.noreply.github.com>
2021-04-21 13:57:20 +0200

    commit the great2

--- TCPsupremacy/Program.cs
+++ TCPsupremacy/Program.cs
@@ -17,18 +17,23 @@ namespace TCPsupremacy
             //{
                 TcpClient tcp = new TcpClient();
                 tcp.Connect("176.23.96.141", 5050);
+                Console.WriteLine("Connected");
 
                 if (Read(tcp) == "!GO") {
+                Console.WriteLine("Poggers");
                     tcp.Close();
                     TcpClient tcp2 = new TcpClient();
                     tcp2.Connect("176.23.96.141", 5050);
+                    Console.WriteLine("Connedted 2 - kekeke booggggggggggg");
                     string IP = Read(tcp2);
                     int port = Convert.ToInt32(Read(tcp2));
+                    Console.WriteLine(IP + ", " + port);
                     TcpClient tcp3 = new TcpClient();
                     tcp3.Connect(IP, port+1);
                     while (true)
                     {
-                        Byte[] data2 = Encoding.UTF8.GetBytes(Console.ReadLine());
+                    Console.WriteLine("Lort");    
+                    Byte[] data2 = Encoding.UTF8.GetBytes(Console.ReadLine());
                         tcp.GetStream().Write(data2, 0, data2.Length);
                         Console.WriteLine(Read(tcp3));
                     }
