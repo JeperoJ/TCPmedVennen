6dc4506aab1c32b1109213b96fba0fa60e586b5c
JeperoJ <55749013+JeperoJ@users.noreply.github.com>
2021-04-21 13:14:03 +0200

    verry cool

---
+++ README.md
@@ -0,0 +1,2 @@
+# TCP-Holepunch
+Læs titlen din ordlblinde spastiske møgsvin af en luder jeg gider dig faktisk ikke
---
+++ TCPsupremacy/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+namespace TCPsupremacy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                TcpClient tcp = new TcpClient();
+                tcp.Connect("176.23.96.141", 5050);
+                Console.WriteLine("Connected");
+                tcp.Close();
+                Console.WriteLine("Dis");
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
